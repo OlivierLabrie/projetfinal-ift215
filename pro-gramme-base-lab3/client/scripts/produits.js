c81243d56e45d907e61ada2cf4276ff80503d853
Lower-Republic9880 <88858653+Pyropingouin@users.noreply.github.com>
2022-11-26 16:52:57 -0500

    Commit final samedi

--- pro-gramme-base-lab3/client/scripts/produits.js
+++ pro-gramme-base-lab3/client/scripts/produits.js
@@ -147,15 +147,18 @@ function enleverItem(item){
         }})
 }
 
+
 function load_panier(item) {
+
 
     image = $('<img src="../images/' + item.nomProduit + '.png" style = "border-right: 2px solid #666DF2"/>');
     nom = $('<td></td>').append(item.nomProduit);
     prix = $('<td></td>').append(item.prix);
-    qte = $('<td><button onclick="ajouterItem('+ item.id + ')">+</button><button onclick="enleverItem('+ item.id + ')">-</button></td>').append(item.quantite);
+    qte = $('<td><button onclick="ajouterItem('+ item.id + ')">+</button><button onclick="enleverItem('+ item.id + ')">-</button>             </td>').append(item.quantite);
     total = $('<td></td>').append(Math.round(item.quantite * item.prix * 100) / 100);
     trash = $('<td></td>')
         .append('<button type="button" class="btn" onClick="remove_item([' + item.id + '])"><span class="bi bi-trash" aria-hidden="true"></span></button>')
+
 
     return $('<tr style = "border: 4px solid #666DF2"></tr>').append(image).append(nom).append(prix).append(qte).append(total).append(trash);
 }
