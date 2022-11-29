8260ffbf05561fc310182958b0d3ef80688cecc0
[chak1001] <[chak1001@usherbrooke.ca]>
2022-11-29 17:11:04 -0500

    enlever section notre histoire dans À propos

--- pro-gramme-base-lab3/client/scripts/inscription.js
+++ pro-gramme-base-lab3/client/scripts/inscription.js
@@ -49,8 +49,6 @@ function versCommande(){
     });
 }
 
-
-
 /**
  * Fonction qui initie le lancement des fonctions de ce script. Appelée par "chargerSousContenu" dans navigation.js.
  * Remplace le DOMContentLoaded qui est lancé bien avant que le contenu associé à ce script ne soit dans l'écran.
