95d1172d1142f8c241851eb6c5957cfada66b61c
[chak1001] <[chak1001@usherbrooke.ca]>
2022-11-30 00:31:01 -0500

    ajout de la fonction pour ajouter des nouveaux clients et il y a un message de validation

--- pro-gramme-base-lab3/client/scripts/inscription.js
+++ pro-gramme-base-lab3/client/scripts/inscription.js
@@ -21,10 +21,34 @@ function attacherListenerMenuGauche(){
     }
 }
 
-function validateConnexion(){
-    var error = document.getElementById("courriel").value;
-}
+function inscrireNewClient(){
+    let PRENOM = document.getElementById("prenom").value;
+    let NOM = document.getElementById("nom").value;
+    let AGE = document.getElementById("age").value;
+    let ADRESSE = document.getElementById("adresse").value;
+    let PAYS = document.getElementById("pays").value;
+    let COURRIEL = document.getElementById("sonCourriel").value;
+    let MDP = document.getElementById("motDePasse").value;
 
+    $.ajax({
+        url: "/clients/",
+        method: "POST",
+        data:JSON.stringify({"mdp":MDP,"prenom":PRENOM,"nom":NOM,"age":AGE,"adresse":ADRESSE,"pays":PAYS,"courriel":COURRIEL}),
+        contentType: "application/json",
+        success: function(result){
+            document.getElementById(`messageValide`).setAttribute("style","display:block")
+            setTimeout(()=>{
+                document.getElementById(`messageValide`).setAttribute("style","display:none")
+            },3000)
+        },
+        error: function(result){
+            document.getElementById(`messageInvalide`).setAttribute("style","display:block")
+            setTimeout(()=>{
+                document.getElementById(`messageInvalide`).setAttribute("style","display:none")
+            },3000)
+        }
+    });
+}
 
 function versCommande(){
     let COURRIEL = document.getElementById("courriel").value;
